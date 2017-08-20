390f9b38486b5ca4efbc89b90f5377988d36e6a8
Nitesh Isave <nitesh.isave@infinxinc.com>
2017-08-20 20:10:33 +0530

    Cloud recogn.

--- ARVideoDemo/helloar.m
+++ ARVideoDemo/helloar.m
@@ -8,8 +8,7 @@
 
 #import "helloar.h"
 
-#import "VideoRenderer.h"
-#import "ARVideo.h"
+#import "BoxRenderer.h"
 
 #import <easyar/types.oc.h>
 #import <easyar/camera.oc.h>
@@ -18,37 +17,49 @@
 #import <easyar/imagetracker.oc.h>
 #import <easyar/imagetarget.oc.h>
 #import <easyar/renderer.oc.h>
+#import <easyar/cloud.oc.h>
 
 #include <OpenGLES/ES2/gl.h>
 
-easyar_CameraDevice * camera = nil;
-easyar_CameraFrameStreamer * streamer = nil;
-NSMutableArray<easyar_ImageTracker *> * trackers = nil;
-easyar_Renderer * videobg_renderer = nil;
-NSMutableArray<VideoRenderer *> * video_renderers = nil;
-VideoRenderer * current_video_renderer = nil;
-int tracked_target = 0;
-int active_target = 0;
-ARVideo * video = nil;
+NSString * cloud_server_address = @"af6993a6d04a78afd20f7991c41ba0da.na1.crs.easyar.com:8080";
+NSString * cloud_key = @"ada1c060e926aae5262ea9fcae345c22";
+NSString * cloud_secret = @"CvFZ1GZcMomw5kw2eOMIkT8Rl8ESwFkBYi1monyF1tVXbBxFBtAEVWx3vty5tuIg6GdCWDNJ619Lj9bSrTlyLCUXmW9fPo7mjcdKuSyVTAYNf4in0llOhWfKfgPpRW95";
+
+easyar_CameraDevice * camera;
+easyar_CameraFrameStreamer * streamer;
+NSMutableArray<easyar_ImageTracker *> * trackers;
+easyar_Renderer * videobg_renderer;
+BoxRenderer * box_renderer;
+easyar_CloudRecognizer * cloud_recognizer;
 bool viewport_changed = false;
 int view_size[] = {0, 0};
 int view_rotation = 0;
 int viewport[] = {0, 0, 1280, 720};
+NSMutableSet<NSString *> * uids;
 
 void loadFromImage(easyar_ImageTracker * tracker, NSString * path)
 {
     easyar_ImageTarget * target = [easyar_ImageTarget create];
     NSString * name = [path substringToIndex:[path rangeOfString:@"."].location];
     NSString * jstr = [@[@"{\n"
-        "  \"images\" :\n"
-        "  [\n"
-        "    {\n"
-        "      \"image\" : \"", path, @"\",\n"
-        "      \"name\" : \"", name, @"\"\n"
-        "    }\n"
-        "  ]\n"
-        "}"] componentsJoinedByString:@""];
+                         "  \"images\" :\n"
+                         "  [\n"
+                         "    {\n"
+                         "      \"image\" : \"", path, @"\",\n"
+                         "      \"name\" : \"", name, @"\"\n"
+                         "    }\n"
+                         "  ]\n"
+                         "}"] componentsJoinedByString:@""];
     [target setup:jstr storageType:(easyar_StorageType_Assets | easyar_StorageType_Json) name:@""];
+    [tracker loadTarget:target callback:^(easyar_Target * target, bool status) {
+        NSLog(@"load target (%d): %@ (%d)", status, [target name], [target runtimeID]);
+    }];
+}
+
+void loadFromJsonFile(easyar_ImageTracker * tracker, NSString * path, NSString * targetname)
+{
+    easyar_ImageTarget * target = [easyar_ImageTarget create];
+    [target setup:path storageType:easyar_StorageType_Assets name:targetname];
     [tracker loadTarget:target callback:^(easyar_Target * target, bool status) {
         NSLog(@"load target (%d): %@ (%d)", status, [target name], [target runtimeID]);
     }];
@@ -68,15 +79,52 @@ BOOL initialize()
     camera = [easyar_CameraDevice create];
     streamer = [easyar_CameraFrameStreamer create];
     [streamer attachCamera:camera];
+    cloud_recognizer = [easyar_CloudRecognizer create];
+    [cloud_recognizer attachStreamer:streamer];
 
     bool status = true;
     status &= [camera open:easyar_CameraDeviceType_Default];
     [camera setSize:[easyar_Vec2I create:@[@1280, @720]]];
+    uids = [[NSMutableSet<NSString *> alloc] init];
+    [cloud_recognizer open:cloud_server_address appKey:cloud_key appSecret:cloud_secret callback_open:^(easyar_CloudStatus status) {
+        if (status == easyar_CloudStatus_Success ) {
+            NSLog(@"CloudRecognizerInitCallBack: Success");
+        } else if (status == easyar_CloudStatus_Reconnecting) {
+            NSLog(@"CloudRecognizerInitCallBack: Reconnecting");
+        } else if (status == easyar_CloudStatus_Fail) {
+            NSLog(@"CloudRecognizerInitCallBack: Fail");
+        } else {
+            NSLog(@"CloudRecognizerInitCallBack: %ld", (long)status);
+        }
+    } callback_recognize:^(easyar_CloudStatus status, NSArray<easyar_Target *> * targets) {
+        if (status == easyar_CloudStatus_Success ) {
+            NSLog(@"CloudRecognizerCallBack: Success");
+        } else if (status == easyar_CloudStatus_Reconnecting) {
+            NSLog(@"CloudRecognizerCallBack: Reconnecting");
+        } else if (status == easyar_CloudStatus_Fail) {
+            NSLog(@"CloudRecognizerCallBack: Fail");
+        } else {
+            NSLog(@"CloudRecognizerCallBack: %ld", (long)status);
+        }
+        @synchronized (uids) {
+            for (easyar_Target * t in targets) {
+                if (![uids containsObject:[t uid]]) {
+                    NSLog(@"add cloud target: %@", [t uid]);
+                    [uids addObject:[t uid]];
+                    [[trackers objectAtIndex:0] loadTarget:t callback:^(easyar_Target *target, bool status) {
+                        NSLog(@"load target (%@): %@ (%d)", status ? @"true" : @"false", [target name], [target runtimeID]);
+                    }];
+                }
+            }
+        }
+    }];
 
     if (!status) { return status; }
     easyar_ImageTracker * tracker = [easyar_ImageTracker create];
     [tracker attachStreamer:streamer];
-    loadAllFromJsonFile(tracker, @"targets.json");
+    loadFromJsonFile(tracker, @"targets.json", @"argame");
+    loadFromJsonFile(tracker, @"targets.json", @"idback");
+    loadAllFromJsonFile(tracker, @"targets2.json");
     loadFromImage(tracker, @"namecard.jpg");
     trackers = [[NSMutableArray<easyar_ImageTracker *> alloc] init];
     [trackers addObject:tracker];
@@ -86,13 +134,9 @@ BOOL initialize()
 
 void finalize()
 {
-    video = nil;
-    tracked_target = 0;
-    active_target = 0;
-
     [trackers removeAllObjects];
-    [video_renderers removeAllObjects];
-    current_video_renderer = nil;
+    cloud_recognizer = nil;
+    box_renderer = nil;
     videobg_renderer = nil;
     streamer = nil;
     camera = nil;
@@ -103,6 +147,7 @@ BOOL start()
     bool status = true;
     status &= (camera != nil) && [camera start];
     status &= (streamer != nil) && [streamer start];
+    status &= (cloud_recognizer != nil) && [cloud_recognizer start];
     [camera setFocusMode:easyar_CameraDeviceFocusMode_Continousauto];
     for (easyar_ImageTracker * tracker in trackers) {
         status &= [tracker start];
@@ -116,6 +161,7 @@ BOOL stop()
     for (easyar_ImageTracker * tracker in trackers) {
         status &= [tracker stop];
     }
+    status &= (cloud_recognizer != nil) && [cloud_recognizer stop];
     status &= (streamer != nil) && [streamer stop];
     status &= (camera != nil) && [camera stop];
     return status;
@@ -123,21 +169,9 @@ BOOL stop()
 
 void initGL()
 {
-    if (active_target != 0) {
-        [video onLost];
-        video = nil;
-        tracked_target = 0;
-        active_target = 0;
-    }
-    videobg_renderer = nil;
     videobg_renderer = [easyar_Renderer create];
-    video_renderers = [[NSMutableArray<VideoRenderer *> alloc] init];
-    for (int k = 0; k < 3; k += 1) {
-        VideoRenderer * video_renderer = [[VideoRenderer alloc] init];
-        [video_renderer init_];
-        [video_renderers addObject:video_renderer];
-    }
-    current_video_renderer = nil;
+    box_renderer = [BoxRenderer alloc];
+    [box_renderer init_];
 }
 
 void resizeGL(int width, int height)
@@ -199,56 +233,16 @@ void render()
         [videobg_renderer render:frame viewport:[easyar_Vec4I create:@[[NSNumber numberWithInt:viewport[0]], [NSNumber numberWithInt:viewport[1]], [NSNumber numberWithInt:viewport[2]], [NSNumber numberWithInt:viewport[3]]]]];
     }
 
-    NSArray<easyar_TargetInstance *> * targetInstances = [frame targetInstances];
-    if ([targetInstances count] > 0) {
-        easyar_TargetInstance * targetInstance = [targetInstances objectAtIndex:0];
-        easyar_Target * target = [targetInstance target];
-        int status = [targetInstance status];
+    for (easyar_TargetInstance * targetInstance in [frame targetInstances]) {
+        easyar_TargetStatus status = [targetInstance status];
         if (status == easyar_TargetStatus_Tracked) {
-            int runtimeID = [target runtimeID];
-            if (active_target != 0 && active_target != runtimeID) {
-                [video onLost];
-                video = nil;
-                tracked_target = 0;
-                active_target = 0;
-            }
-            if (tracked_target == 0) {
-                if (video == nil && [video_renderers count] > 0) {
-                    NSString * target_name = [target name];
-                    if ([target_name isEqualToString:@"argame"] && [[video_renderers objectAtIndex:0] texid] != 0) {
-                        video = [[ARVideo alloc] init];
-                        [video openVideoFile:@"video.mp4" texid:[[video_renderers objectAtIndex:0] texid]];
-                        current_video_renderer = [video_renderers objectAtIndex:0];
-                    } else if ([target_name isEqualToString:@"namecard"] && [[video_renderers objectAtIndex:1] texid] != 0) {
-                        video = [[ARVideo alloc] init];
-                        [video openTransparentVideoFile:@"transparentvideo.mp4" texid:[[video_renderers objectAtIndex:1] texid]];
-                        current_video_renderer = [video_renderers objectAtIndex:1];
-                    } else if ([target_name isEqualToString:@"idback"] && [[video_renderers objectAtIndex:2] texid] != 0) {
-                        video = [[ARVideo alloc] init];
-                        [video openStreamingVideo:@"https://sightpvideo-cdn.sightp.com/sdkvideo/EasyARSDKShow201520.mp4" texid:[[video_renderers objectAtIndex:2] texid]];
-                        current_video_renderer = [video_renderers objectAtIndex:2];
-                    }
-                }
-                if (video != nil) {
-                    [video onFound];
-                    tracked_target = runtimeID;
-                    active_target = runtimeID;
-                }
-            }
+            easyar_Target * target = [targetInstance target];
             easyar_ImageTarget * imagetarget = [target isKindOfClass:[easyar_ImageTarget class]] ? (easyar_ImageTarget *)target : nil;
-            if (imagetarget != nil) {
-                if (current_video_renderer != nil) {
-                    [video update];
-                    if ([video isRenderTextureAvailable]) {
-                        [current_video_renderer render:[camera projectionGL:0.2f farPlane:500.f] cameraview:[targetInstance poseGL] size:[imagetarget size]];
-                    }
-                }
+            if (imagetarget == nil) {
+                continue;
             }
-        }
-    } else {
-        if (tracked_target != 0) {
-            [video onLost];
-            tracked_target = 0;
+            video
+            [box_renderer render:[camera projectionGL:0.2f farPlane:500.f] cameraview:[targetInstance poseGL] size:[imagetarget size]];
         }
     }
 }
